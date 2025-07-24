1aedaad282857c939740b4de5c275a5c1ba73022
venturasdev <venturas.dev2018@gmail.com>
2025-07-24 10:33:05 -0300

    master

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     {/*Navbar*/}
+     {/*destaque*/}
+     {/*Em alta*/}
+     {/*Filmes de cada categoria*/}
+    </div>
+  );
+}
+
+export default App;
